a29f648ba216869df989fe087c47d21335a6194d
Sam Devine <samuel.will.devine@gmail.com>
2022-02-03 16:59:18 -0700

    create requirements.txt and use PG DB

--- free_tomorrow_backend/settings.py
+++ free_tomorrow_backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'free_tomorrow_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'free_tomorrow_backend_development',
+        'USER': 'postgres',
+        'PASSWORD': 'free_tomorrow_backend_development',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
