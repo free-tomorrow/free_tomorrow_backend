08fd1fd996f553a8b963ec8fb04e6536fca693a3
Sam Devine <samuel.will.devine@gmail.com>
2022-02-03 18:15:27 -0700

    hide secret key

--- free_tomorrow_backend/settings.py
+++ free_tomorrow_backend/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$@7db#0n!-pout=d(!6wbon#ksffv65(s7+^kz_x$*5)8=fg00'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
