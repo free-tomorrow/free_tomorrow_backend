6d8f9b8eadb80792c38381272a7e9d8c643fbedd
Gregory Fischer <67120586+GregoryJFischer@users.noreply.github.com>
2022-02-06 17:04:10 -0500

    Feat: add trips table with basic endpoints

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Trip
 
 admin.site.register(User)
+admin.site.register(Trip)
--- api/views.py
+++ api/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets
+from rest_framework.response import Response
 
-from .serializers import UserSerializer
-from .models import User
+from .serializers import UserSerializer, UserTripSerializer, TripSerializer
+from .models import User, Trip
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('username')
+    queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserTripSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+
+    def list(self, request):
+        queryset = Trip.objects.all()
+        serializer = TripSerializer(queryset, many=True)
+        return Response(serializer.data)
+
