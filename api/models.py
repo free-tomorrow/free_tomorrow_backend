3267c646edca4c0e0f4b41800ce07585d9f255f1
Gregory Fischer <67120586+GregoryJFischer@users.noreply.github.com>
2022-02-06 18:00:30 -0500

    Feat: add validation for email uniqueness

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=64)
-    email = models.CharField(max_length=64)
+    email = models.CharField(max_length=64, unique= True)
 
     def __str__(self):
         return self.email
