b686ed0baeaa2dbf74b0d0b986995cfd4396a4e9
Gregory Fischer <67120586+GregoryJFischer@users.noreply.github.com>
2022-02-07 12:20:26 -0500

    Refactor: remove useless code

--- api/views.py
+++ api/views.py
@@ -11,6 +11,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def detail(self, request, user_id):
+        import ipdb; ipdb.set_trace()
+        query = User.objects.get(pk = user_id)
+        serializer = UserTripSerializer(query)
+        return Response(serializer.data)
+
     def list(self, request):
         queryset = User.objects.all()
         serializer = UserTripSerializer(queryset, many=True)
@@ -19,9 +25,3 @@ class UserViewSet(viewsets.ModelViewSet):
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
-
-    def list(self, request):
-        queryset = Trip.objects.all()
-        serializer = TripSerializer(queryset, many=True)
-        return Response(serializer.data)
-
