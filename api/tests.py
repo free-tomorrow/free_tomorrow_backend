ff209f90155fd232a9911e2609b55ae956c7f5f2
Sam Devine <samuel.will.devine@gmail.com>
2022-02-07 12:06:17 -0700

    test user email uniqueness

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,21 @@
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from api.models import User, Trip
 # TestCase is a subclass of unittest, the default Python testing framework
 
 class URLTests(TestCase):
     def test_root_path(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+class ModelTests(TestCase):
+    def test_email_uniqueness(self):
+        user_1 = User.objects.create(name='Sam', email='swd1@gmail.com')
+        user_2 = User.objects.create(name='Samuel', email='swd1@gmail.com')
+
+        with self.assertRaises(ValidationError):
+            user_2.save()
+            user_2.full_clean()
+            # This line is required here because Django doesn't run full
+            # validation on save. full_clean() manually runs a full validation
