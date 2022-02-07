6af6e652e7954a6fee417568a7faea0ce4eb9577
Sam Devine <samuel.will.devine@gmail.com>
2022-02-07 13:20:55 -0700

    refactor uniqueness test

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,4 @@
 from django.db import IntegrityError
-from django.core.exceptions import ValidationError
 from django.test import TestCase
 from api.models import User, Trip
 # TestCase is a subclass of unittest, the default Python testing framework
@@ -12,10 +11,8 @@ class URLTests(TestCase):
 class ModelTests(TestCase):
     def test_email_uniqueness(self):
         user_1 = User.objects.create(name='Sam', email='swd1@gmail.com')
-        user_2 = User.objects.create(name='Samuel', email='swd1@gmail.com')
 
-        with self.assertRaises(ValidationError):
-            user_2.save()
-            user_2.full_clean()
-            # This line is required here because Django doesn't run full
-            # validation on save. full_clean() manually runs a full validation
+        # this context block allows us to assert that an exception will be
+        # raised from the code inside of it (email is not unique)
+        with self.assertRaises(IntegrityError):
+            User.objects.create(name='Samuel', email='swd1@gmail.com')
