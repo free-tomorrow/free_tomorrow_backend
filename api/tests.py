b51f2c160cb404bec871e46fb265401025a13e9a
Sam Devine <samuel.will.devine@gmail.com>
2022-02-07 11:45:44 -0700

    test: root path

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+# TestCase is a subclass of unittest, the default Python testing framework
 
-# Create your tests here.
+class URLTests(TestCase):
+    def test_root_path(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
