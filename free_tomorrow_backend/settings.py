d14a6e4574364b1c7741d0b65013879cf70865fc
Gregory Fischer <67120586+GregoryJFischer@users.noreply.github.com>
2022-02-10 15:26:35 -0500

    Feat: add settings for CORS

--- free_tomorrow_backend/settings.py
+++ free_tomorrow_backend/settings.py
@@ -39,10 +39,14 @@ ALLOWED_HOSTS = [
     'free-tomorrow-be.herokuapp.com'
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,6 +59,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
